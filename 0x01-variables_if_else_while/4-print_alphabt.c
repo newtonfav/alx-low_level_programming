080c4a65281cc9ff0b4a9d1b05cdcad4e280de33
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-14 13:17:44 -0700

    updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - lsls
+ *
+ * Return: ldld
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'j'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
