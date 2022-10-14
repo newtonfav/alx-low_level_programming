d73ba5bf3ba777c267315505f58d95d6ec9f9924
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-14 13:27:45 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - endll
+ *
+ * Return: dldl
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
