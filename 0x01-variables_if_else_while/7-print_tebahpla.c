d57a38909dbfe90c4e52c813b28b5bc0330128ef
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-14 13:38:07 -0700

    updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - ldld
+ *
+ * Return: kfkf
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
