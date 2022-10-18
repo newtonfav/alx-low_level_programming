a32b5bb344a4c4b199b470b0e2d2362b16ce4427
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-18 09:05:50 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
+	char print_alphabet(void)
 	{
-		_putchar(i);
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 	return (0);
 }
