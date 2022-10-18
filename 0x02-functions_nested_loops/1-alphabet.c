97d74d15b178ddd47e4fdb7104507fa03c45fedb
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-18 09:10:49 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,11 @@
  *
  * Return: dlld
  */
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
-	return (0);
+	_putchar('\n');
 }
