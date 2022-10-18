7e5769071a888b8d65ebd012bc8d9b2a70d64033
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-18 09:24:55 -0700

    updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - dldld
+ *
+ * Return: dldlld
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
