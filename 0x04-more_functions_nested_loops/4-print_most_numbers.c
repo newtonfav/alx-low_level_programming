6b2b15a1fb33e069e360476e12248e6936912ffb
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-20 16:45:12 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 -9 except 2 & 4
+ *
+ * Return: !:'smsm
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		if (i == 2)
+			continue;
+		_putchar(i);
+	}
+}
