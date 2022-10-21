5780d7c5e303244144ed80784944df9e30dc9279
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-20 19:01:56 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.b"
+/**
+ * print_triangle - &:&:'mss
+ *
+ * Return: jndndnd
+ * @size: &//'sms
+ */
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
