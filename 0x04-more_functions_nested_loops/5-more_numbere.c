009e92686833a9cf7b1205cb00d18dd6fe2b7c2c
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-20 17:16:56 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/5-more_numbere.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - jsnsnsnz
+ *
+ * Return: jsjd
+ */
+void more_numbers(void)
+{
+	char i, j;
+
+	for (i = '0'; i <= '10'; i++)
+	{
+		for (j = '0'; j <= '14'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
