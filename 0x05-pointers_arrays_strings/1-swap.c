48e092f22fa505cf455c92d02d7bef5c38e08d0a
Favour Oghenekowho <gh.oghene@gmail.com>
2022-10-24 14:19:33 -0700

    updated

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap two values
+ * @a: paramater
+ * @b: paramater
+ */
+void swap_int(int *a, int *b)
+{
+	int c, d;
+
+	a = c;
+	b = d;
+	*a = c;
+	*b = d;
+}
